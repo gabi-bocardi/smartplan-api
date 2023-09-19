d722ae44eaf1025b37b04635a0552403d12170de
Vlad-Vekslyer <bocardigm@gmail.com>
2023-09-18 21:17:44 -0700

    installed and started on firebase initialization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './controllers/auth.controller';
-import { AuthService } from './services/auth.service';
 import { ConfigModule } from '@nestjs/config'
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AuthController],
-  providers: [AuthService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './services/prisma.service';
 
-async function bootstrap() {
+async function main() {
+  const firebaseConfig = {
+
+  };
+
   const app = await NestFactory.create(AppModule, {
     logger: ['debug', 'error', 'warn', 'log']
   });
@@ -11,4 +15,4 @@ async function bootstrap() {
   await app.listen(5000);
   console.log('Listening on', await app.getUrl());
 }
-bootstrap();
+main();
